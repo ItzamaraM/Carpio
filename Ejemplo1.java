7af19dc61dc1be8bcabeba224714c87980d80218
ItzamaraM <32503853+ItzamaraM@users.noreply.github.com>
2018-08-22 22:47:57 -0500

    Create Ejemplo1.java

---
+++ Ejemplo1.java
@@ -0,0 +1,14 @@
+import javax.swing.ImageIcon;
+  class Demo{
+    ImageIcon x;
+    
+  public static void main(String[]y){
+        Demo app = new Demo();
+        
+        app.x = new ImageIcon("Iron Maiden.gif");
+        
+        new javax.swing.JOptionPane().showMessageDialog(null, "eddie", "Iron Maiden", -1, app.x);
+        
+       }
+       
+  }
