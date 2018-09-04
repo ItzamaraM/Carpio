0ffa52bbbba02f9121ecbe2850b2cbac39fdd363
ItzamaraM <32503853+ItzamaraM@users.noreply.github.com>
2018-09-04 11:18:53 -0500

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,20 @@
+
+package carpio.Unidad1;
+
+public class Demo {
+
+    
+    public static void main(String[] args) {
+        
+        class Estudiante{
+            String numControl;
+        }
+        class EstudianteISC extends Estudiante{
+            public EstudianteISC(){
+                numControl = "no asignado";
+                
+            }
+        }
+    }
+    
+}
