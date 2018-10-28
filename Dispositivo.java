38dd46c449c2766212ca645f76d2935825440ce0
ItzamaraM <32503853+ItzamaraM@users.noreply.github.com>
2018-10-28 16:34:40 -0600

    Create Dispositivo.java

---
+++ Dispositivo.java
@@ -0,0 +1,21 @@
+/*
+   Dispositivo.java
+   Jueves veinticinco de octubre de dosmil dieciocho
+   (R) Tecnológico Nacional de México Campus León 
+   -------------------------------------------------
+    
+    Interface para capturar y mostrar Strings
+
+*/
+
+package poo.segundoParcial;
+
+/**
+ *
+ * @author José Gerardo Carpio Flores
+ */
+
+public interface Dispositivo {
+    public String input(String s);
+    public void print(String s);
+}
